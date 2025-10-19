fe4e59d1162bd240f12c65f2c6f24ff4a096d0d2
Sega <brajicsergej@gmail.com>
2025-10-19 22:49:54 +0200

    LDA instruction set

--- src/cpu/6502ricoh.cpp
+++ src/cpu/6502ricoh.cpp
@@ -16,6 +16,157 @@ void CPU::reset()
     NMI = IRQ = RESET = false;
 }
 
+void CPU::ldaImmediate()
+{
+    uint8_t value = bus->read(pc++);
+    A = value;
+
+    if ((A & 0x80) != 0)
+        P |= 0x80;
+    else
+        P &= ~0x80;
+    if (A == 0)
+        P |= 0x02;
+    else
+        P &= ~0x02;
+}
+
+void CPU::ldaZeroPage()
+{
+    uint8_t address = bus->read(pc++);
+    uint8_t value = bus->read(address);
+
+    A = value;
+
+    if ((A & 0x80) != 0)
+        P |= 0x80;
+    else
+        P &= ~0x80;
+    if (A == 0)
+        P |= 0x02;
+    else
+        P &= ~0x02;
+}
+
+void CPU::ldaZeroPageX()
+{
+    uint8_t address = bus->read(pc++);
+    uint8_t zeroPageAddress = (address + X) & 0xFF;
+    uint8_t value = bus->read(zeroPageAddress);
+
+    A = value;
+
+    if ((A & 0x80) != 0)
+        P |= 0x80;
+    else
+        P &= ~0x80;
+    if (A == 0)
+        P |= 0x02;
+    else
+        P &= ~0x02;
+}
+
+void CPU::ldaAbsolute()
+{
+    uint8_t lowByte = bus->read(pc++);
+    uint8_t highByte = bus->read(pc++);
+    uint16_t finalAddress = (highByte << 8) | lowByte;
+    uint8_t value = bus->read(finalAddress);
+
+    A = value;
+    if ((A & 0x80) != 0)
+        P |= 0x80;
+    else
+        P &= ~0x80;
+    if (A == 0)
+        P |= 0x02;
+    else
+        P &= ~0x02;
+}
+
+void CPU::ldaAbsoluteX()
+{
+    uint8_t lowByte = bus->read(pc++);
+    uint8_t highByte = bus->read(pc++);
+    uint16_t finalAddress = (highByte << 8) | lowByte;
+    uint16_t finalAddressX = finalAddress + X;
+    uint8_t value = bus->read(finalAddressX);
+
+    A = value;
+
+    if ((finalAddress & 0xFF00) != (finalAddressX & 0xFF00))
+        cycles++;
+
+    A = value;
+    if ((A & 0x80) != 0)
+        P |= 0x80;
+    else
+        P &= ~0x80;
+    if (A == 0)
+        P |= 0x02;
+    else
+        P &= ~0x02;
+}
+
+void CPU::ldaAbsoluteY()
+{
+    uint8_t lowByte = bus->read(pc++);
+    uint8_t highByte = bus->read(pc++);
+    uint16_t finalAddress = (highByte << 8) | lowByte;
+    uint16_t finalAddressY = finalAddress + Y;
+    uint8_t value = bus->read(finalAddressY);
+
+    A = value;
+
+    if ((finalAddress & 0xFF00) != (finalAddressY & 0xFF00))
+        cycles++;
+
+    if ((A & 0x80) != 0)
+        P |= 0x80;
+    else
+        P &= ~0x80;
+    if (A == 0)
+        P |= 0x02;
+    else
+        P &= ~0x02;
+}
+
+void CPU::ldaIndexedIndirect()
+{
+    uint8_t address = bus->read(pc++);
+    uint8_t zeroPageAddress = (address + X) & 0xFF;
+    uint8_t lowByte = bus->read(zeroPageAddress);
+    uint8_t highByte = bus->read((zeroPageAddress + 1) & 0xFF);
+    uint16_t ptr = (highByte << 8) | lowByte;
+    A = bus->read(ptr);
+
+    if ((A & 0x80) != 0)
+        P |= 0x80;
+    else
+        P &= ~0x80;
+    if (A == 0)
+        P |= 0x02;
+    else
+        P &= ~0x02;
+}
+void CPU::ldaIndirectIndexed()
+{
+    uint8_t address = bus->read(pc++);
+    uint8_t lowByte = bus->read(address);
+    uint8_t highByte = bus->read((address + 1) & 0xFF);
+    uint16_t ptr = (highByte << 8) | lowByte;
+    uint16_t finalAddress = ptr + Y;
+    A = bus->read(finalAddress);
+
+    if ((A & 0x80) != 0)
+        P |= 0x80;
+    else
+        P &= ~0x80;
+    if (A == 0)
+        P |= 0x02;
+    else
+        P &= ~0x02;
+}
 void CPU::clock()
 {
     if (cycles == 0)
