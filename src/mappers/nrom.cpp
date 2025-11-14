5eecd9b0b7ba45f54795a2713352a4ffecbd1490
Sega <brajicsergej@gmail.com>
2025-11-14 16:54:49 +0100

    Defined mapper files and defined structure

---
+++ src/mappers/nrom.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+class Mapper
+{
+public:
+    virtual uint8_t cpuRead(uint16_t addr) = 0;
+    virtual void cpuWrite(uint16_t addr, uint8_t data) = 0;
+
+    virtual uint8_t ppuRead(uint16_t addr) = 0;
+    virtual void ppuWrite(uint16_t addr, uint8_t data) = 0;
+
+    virtual ~Mapper();
+};
